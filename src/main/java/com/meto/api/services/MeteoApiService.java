34a79c21f24b4d6922da3481563c48fbe8c76053
mg9700 <ghoraf.mouhsin@gmail.com>
2025-01-05 01:21:49 +0100

    add docker compose

--- src/main/java/com/meto/api/services/MeteoApiService.java
+++ src/main/java/com/meto/api/services/MeteoApiService.java
@@ -2,21 +2,22 @@ package com.meto.api.services;
 
 
 import com.meto.api.dto.MeteoDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 @Service
+@Slf4j
 public class MeteoApiService {
 
 
     private final WebClient webClient;
-
-    private static final String URL_METEO_LOCAL="http://localhost:8080";
-
-    public MeteoApiService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl(URL_METEO_LOCAL).build();
+    public MeteoApiService(WebClient.Builder webClientBuilder, @Value("${meteo.api.url}") String meteoApiUrl) {
+        log.info("MeteoApiService URL: {}", meteoApiUrl);
+        this.webClient = webClientBuilder.baseUrl(meteoApiUrl).build();
     }
     public Mono<MeteoDto> getMeteo(String location) {
         return webClient.get()
